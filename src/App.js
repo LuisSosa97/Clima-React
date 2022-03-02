229970eb06f7b8d0f9397dd973dee43d194bd607
LuisSosa97 <63810870+LuisSosa97@users.noreply.github.com>
2022-03-02 00:02:13 -0600

    v0.0.3

--- src/App.js
+++ src/App.js
@@ -1,11 +1,27 @@
 import React, { useState, useEffect } from 'react';
+
 //componentes
 import Header from './components/Header';
 import Formulario from './components/Formulario';
 import Error from './components/Error';
 import ResultadoClima from './components/ResultadoClima';
+import { supabase } from './components/supabaseClient'
+import Auth from './components/auth/Auth'
+import Account from './components/account/Account'
+
+
+
 
 function App() {
+  const [session, setSession] = useState(null)
+
+  useEffect(() => {
+    setSession(supabase.auth.session())
+
+    supabase.auth.onAuthStateChange((_event, session) => {
+      setSession(session)
+    })
+  }, [])
 
   //State principal
   //ciudad = state, guardarCiudad = this.setState()
@@ -74,26 +90,8 @@ function App() {
   }
 
   return (
-    <div className="App">
-      <Header 
-      titulo="Clima con React"
-      />
-
-      <div className="contenedor-form">
-        <div className="container">
-          <div className="row">
-            <div className="col s12 m6">
-              <Formulario 
-                datosConsulta={datosConsulta}
-              />
-            </div>
-
-            <div className="col s12 m6">
-              {componente}
-            </div>
-          </div>
-        </div>
-      </div>
+    <div className="container" style={{ padding: '50px 0 100px 0' }}>
+      {!session ? <Auth /> : <Account key={session.user.id} session={session} />}
     </div>
   );
 }
