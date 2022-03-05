a7b097d0fe9cd0f2e960ccceb5a25d7c69f370d8
karla16Santoyo99 <64182574+karla16Santoyo99@users.noreply.github.com>
2022-03-04 20:06:41 -0600

    Imagenes

--- src/components/Carousel/Carousel.jsx
+++ src/components/Carousel/Carousel.jsx
@@ -3,40 +3,40 @@ import { Carousel } from 'react-carousel-minimal';
 function App() {
  const data = [
     {
-      image: "https://upload.wikimedia.org/wikipedia/commons/thumb/0/0c/GoldenGateBridge-001.jpg/1200px-GoldenGateBridge-001.jpg",
-      caption: "San Francisco"
+      image: "https://www.elagoradiario.com/wp-content/uploads/2019/06/sequ%C3%ADa-1140x600.jpg",
+      caption: "Sequias"
     },
     {
-      image: "https://cdn.britannica.com/s:800x450,c:crop/35/204435-138-2F2B745A/Time-lapse-hyper-lapse-Isle-Skye-Scotland.jpg",
-      caption: "Scotland"
+      image: "https://pbs.twimg.com/media/Cgc9DNUUsAA8tph.jpg",
+      caption: "Climatología"
     },
     {
-      image: "https://static2.tripoto.com/media/filter/tst/img/735873/TripDocument/1537686560_1537686557954.jpg",
-      caption: "Darjeeling"
+      image: "https://www.caracteristicas.co/wp-content/uploads/2018/11/tormenta-electrica-e1583114555455.jpg",
+      caption: "Tormentas"
     },
     {
-      image: "https://upload.wikimedia.org/wikipedia/commons/thumb/1/16/Palace_of_Fine_Arts_%2816794p%29.jpg/1200px-Palace_of_Fine_Arts_%2816794p%29.jpg",
-      caption: "San Francisco"
+      image: "https://www.sostenibilidad.com/media/501094/desastres-naturales.jpg",
+      caption: "Cambios Climaticos"
     },
     {
-      image: "https://i.natgeofe.com/n/f7732389-a045-402c-bf39-cb4eda39e786/scotland_travel_4x3.jpg",
-      caption: "Scotland"
+      image: "https://cdn.forbes.com.mx/2019/06/Paris-calor-unsplash-ciudad-640x360.jpg",
+      caption: "Calor"
     },
     {
-      image: "https://www.tusktravel.com/blog/wp-content/uploads/2020/07/Best-Time-to-Visit-Darjeeling-for-Honeymoon.jpg",
-      caption: "Darjeeling"
+      image: "https://www.eluniversal.com.mx/sites/default/files/2020/11/03/frente_frio_0.jpg",
+      caption: "Frios"
     },
     {
-      image: "https://www.omm.com/~/media/images/site/locations/san_francisco_780x520px.ashx",
-      caption: "San Francisco"
+      image: "https://static.diariofemenino.com/uploads/psicologia/214120-sonar-montana.jpg",
+      caption: "Nevadas"
     },
     {
-      image: "https://images.ctfassets.net/bth3mlrehms2/6Ypj2Qd3m3jQk6ygmpsNAM/61d2f8cb9f939beed918971b9bc59bcd/Scotland.jpg?w=750&h=422&fl=progressive&q=50&fm=jpg",
-      caption: "Scotland"
+      image: "https://www.lifeder.com/wp-content/uploads/2021/10/clima-tipos.jpg",
+      caption: "Climas"
     },
     {
-      image: "https://www.oyorooms.com/travel-guide/wp-content/uploads/2019/02/summer-7.jpg",
-      caption: "Darjeeling"
+      image: "https://www.caracteristicas.co/wp-content/uploads/2018/08/clima-mediterraneo-grecia-800x404.jpg",
+      caption: "Climas "
     }
   ];
 
