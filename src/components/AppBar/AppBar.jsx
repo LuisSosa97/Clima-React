c7c97824db511a4412011f9a6706f4c6c904ffa6
karla16Santoyo99 <64182574+karla16Santoyo99@users.noreply.github.com>
2022-03-05 22:16:46 -0600

    cambios css

--- src/components/AppBar/AppBar.jsx
+++ src/components/AppBar/AppBar.jsx
@@ -1,36 +1,83 @@
-import * as React from 'react';
-import AppBar from '@mui/material/AppBar';
-import Box from '@mui/material/Box';
-import Toolbar from '@mui/material/Toolbar';
-import Typography from '@mui/material/Typography';
-import Button from '@mui/material/Button';
-import IconButton from '@mui/material/IconButton';
-import MenuIcon from '@mui/icons-material/Menu';
+import React from 'react';
+import { AppBar, Toolbar, Typography, Box, IconButton, Button } from '@mui/material';
 import { supabase } from "../supabaseClient";
+import { Link } from 'react-router-dom';
+import { cyan } from '@mui/material/colors';
 
-export default function ButtonAppBar() {
-  return (
-    <Box sx={{ flexGrow: 1 }}>
-      <AppBar position="static">
-        <Toolbar>
-          <IconButton
-            size="large"
-            edge="start"
-            color="inherit"
-            aria-label="menu"
-            sx={{ mr: 2 }}
-          >
-            <MenuIcon />
-          </IconButton>
-          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-            Clima
-          </Typography>
-          <Button color="inherit" onClick={() => supabase.auth.signOut()}>
+
+const Navbar = () => {
+
+    return (
+
+        <div>
+
+            <Box sx={{ flexGrow: 1 }}>
+
+                <AppBar position="fixed" sx={{ bgcolor: cyan[700] }}>
+
+                    <Toolbar>
+                        <IconButton
+                            size="large"
+                            edge="start"
+                            color="inherit"
+                            aria-label="menu"
+                            sx={{ mr: 2 }}
+                        >
+
+
+                        </IconButton>
+                        <Typography variant='h6' component="div" sx={{ flexGrow: 1 }}>
+                            <Link to="/">
+                                Inicio
+                            </Link>
+                        </Typography>
+                        <Typography variant='h6' component="div" sx={{ flexGrow: 2 }}>
+
+                        </Typography>
+                       
+
+                        <Button color="inherit">
+                            < Link to="/AddRecordatorio">
+                                Nuevo Recordatorio
+                            </Link>
+                        </Button>
+
+                        <Button>
+                            <a href='https://github.com/karla16Santoyo99/SUPABASE1'>
+                                Github
+                            </a>
+                        </Button>
+
+                        <Button color="inherit" >
+                            <Link to="/Account">
+                                Perfil
+                            </Link>
+                        </Button>
+
+
+
+                        <Button color="inherit" onClick={() => supabase.auth.signOut()}>
                             Salir
                         </Button>
-          
-        </Toolbar>
-      </AppBar>
-    </Box>
-  );
+
+
+
+
+
+
+
+
+
+
+                    </Toolbar>
+
+                </AppBar>
+            </Box>
+
+
+
+        </div>
+    );
 }
+
+export default Navbar
